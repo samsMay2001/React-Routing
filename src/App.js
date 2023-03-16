f3cfbae9f489eef2f801ded4391460a52c390816
sam <samscloud2@gmail.com>
2023-03-16 19:31:10 -0300

    added routes and links

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import AboutMe from './components/AboutMe/AboutMe';
 import HomePage from './components/HomePage/HomePage';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <nav className='nav'>
-        <a href='#' className='nav-item'>Homepage</a>
-        <a href='#' className='nav-item'>About Me</a>
+        <Link to='/' className='nav-item'>Homepage</Link>
+        <Link to='/about-me' className='nav-item'>About me</Link>
       </nav>
-      <HomePage/>
-      <AboutMe/>
+      <Routes>
+        <Route path='/' element= {<HomePage />} />
+        <Route path='/about-me' element= {<AboutMe />} />
+      </Routes>
     </div>
   );
 }
